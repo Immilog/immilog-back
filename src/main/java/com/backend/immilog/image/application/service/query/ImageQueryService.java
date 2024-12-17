119d88de620288e27ba768714d07c6599acbe1b9
bzhs1992 <bzhs1992@icloud.com>
2024-12-17 21:01:38 +0900

    feat (image) : 누락된 @Transactional 어노테이션 추가

--- src/main/java/com/backend/immilog/image/application/service/query/ImageQueryService.java
+++ src/main/java/com/backend/immilog/image/application/service/query/ImageQueryService.java
@@ -4,12 +4,14 @@ import com.backend.immilog.image.domain.model.Image;
 import com.backend.immilog.image.domain.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ImageQueryService {
     private final ImageRepository imageRepository;
 
+    @Transactional(readOnly = true)
     public Image getImageByPath(String imagePath) {
         return imageRepository.findByPath(imagePath);
     }
