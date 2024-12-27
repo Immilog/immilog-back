8a75f376a59f36b5b36e4644f2b22d200f38c8f0
bzhs1992 <bzhs1992@icloud.com>
2024-12-27 21:00:25 +0900

    feat (user) : 분산락 어노테이션 대체

--- src/main/java/com/backend/immilog/user/application/services/UserReportService.java
+++ src/main/java/com/backend/immilog/user/application/services/UserReportService.java
@@ -1,6 +1,6 @@
 package com.backend.immilog.user.application.services;
 
-import com.backend.immilog.global.infrastructure.persistence.lock.RedisDistributedLock;
+import com.backend.immilog.global.aop.lock.DistributedLock;
 import com.backend.immilog.user.application.command.UserReportCommand;
 import com.backend.immilog.user.application.services.command.ReportCommandService;
 import com.backend.immilog.user.application.services.command.UserCommandService;
@@ -13,8 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 import static com.backend.immilog.user.domain.enums.ReportReason.OTHER;
 import static com.backend.immilog.user.exception.UserErrorCode.*;
 
@@ -26,80 +24,39 @@ public class UserReportService {
     private final UserCommandService userCommandService;
     private final ReportCommandService reportCommandService;
     private final ReportQueryService reportQueryService;
-    private final RedisDistributedLock redisDistributedLock;
 
     public UserReportService(
             UserQueryService userQueryService,
             UserCommandService userCommandService,
             ReportCommandService reportCommandService,
-            ReportQueryService reportQueryService,
-            RedisDistributedLock redisDistributedLock
+            ReportQueryService reportQueryService
     ) {
         this.userQueryService = userQueryService;
         this.userCommandService = userCommandService;
         this.reportCommandService = reportCommandService;
         this.reportQueryService = reportQueryService;
-        this.redisDistributedLock = redisDistributedLock;
-    }
-
-    private static Report createReport(
-            Long targetUserSeq,
-            Long reporterUserSeq,
-            UserReportCommand userReportCommand
-    ) {
-        return Report.of(
-                targetUserSeq,
-                reporterUserSeq,
-                userReportCommand,
-                userReportCommand.reason().equals(OTHER)
-        );
     }
 
     @Async
+    @DistributedLock(key = LOCK_KEY, identifier = "#targetUserSeq", expireTime = 5)
     public void reportUser(
             Long targetUserSeq,
             Long reporterUserSeq,
             UserReportCommand userReportCommand
     ) {
-        reportValidation(targetUserSeq, reporterUserSeq);
-        boolean lockAcquired = false;
-        try {
-            lockAcquired = redisDistributedLock.tryAcquireLock(LOCK_KEY, targetUserSeq.toString());
-            if (lockAcquired) {
-                processReport(targetUserSeq, reporterUserSeq, userReportCommand);
-            } else {
-                log.error(
-                        "사용자 신고 실패 - 원인: 락 획득 실패  targetUserSeq: {}, time: {}",
-                        targetUserSeq,
-                        LocalDateTime.now()
-                );
-            }
-        } finally {
-            if (lockAcquired) {
-                redisDistributedLock.releaseLock(LOCK_KEY, targetUserSeq.toString());
-            }
-        }
-    }
-
-    private void processReport(
-            Long targetUserSeq,
-            Long reporterUserSeq,
-            UserReportCommand userReportCommand
-    ) {
+        validateDifferentUsers(targetUserSeq, reporterUserSeq);
+        validateItsNotDuplicatedReport(targetUserSeq, reporterUserSeq);
         User user = getUser(targetUserSeq);
         user.increaseReportedCount();
-        Report report = createReport(targetUserSeq, reporterUserSeq, userReportCommand);
+        Report report = Report.of(
+                targetUserSeq,
+                reporterUserSeq,
+                userReportCommand,
+                userReportCommand.reason().equals(OTHER)
+        );
         reportCommandService.save(report);
         userCommandService.save(user);
         log.info("User {} reported by {}", targetUserSeq, reporterUserSeq);
-    }
-
-    private void reportValidation(
-            Long targetUserSeq,
-            Long reporterUserSeq
-    ) {
-        validateDifferentUsers(targetUserSeq, reporterUserSeq);
-        validateItsNotDuplicatedReport(targetUserSeq, reporterUserSeq);
     }
 
     private void validateItsNotDuplicatedReport(
@@ -124,9 +81,7 @@ public class UserReportService {
         }
     }
 
-    private User getUser(
-            Long targetUserSeq
-    ) {
+    private User getUser(Long targetUserSeq) {
         return userQueryService
                 .getUserById(targetUserSeq)
                 .orElseThrow(() -> new UserException(USER_NOT_FOUND));
