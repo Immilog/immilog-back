5e4a18741c4b7ab86d50b00c5ee77903bb5b3c97
bzhs1992 <bzhs1992@icloud.com>
2025-01-20 21:31:51 +0900

    remove (global) : Jpa의존성 낮추기 위해 BaseEntity 제거

--- src/main/java/com/backend/immilog/image/presentation/controller/ImageController.java
+++ src/main/java/com/backend/immilog/image/presentation/controller/ImageController.java
@@ -1,11 +1,12 @@
 package com.backend.immilog.image.presentation.controller;
 
-import com.backend.immilog.global.model.GlobalApiResponse;
 import com.backend.immilog.image.application.service.ImageService;
 import com.backend.immilog.image.domain.enums.ImageType;
 import com.backend.immilog.image.presentation.request.ImageRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,8 +29,8 @@ public class ImageController {
 
     @PostMapping
     @Operation(summary = "이미지 업로드", description = "이미지를 업로드합니다.")
-    public ResponseEntity<GlobalApiResponse> uploadImage(
-            List<MultipartFile> multipartFile,
+    public ResponseEntity<ImageResponse> uploadImage(
+            @RequestParam("multipartFile") List<MultipartFile> multipartFile,
             @RequestParam("imagePath") String imagePath,
             @RequestParam("imageType") ImageType imageType
     ) {
@@ -37,15 +38,32 @@ public class ImageController {
 
         return ResponseEntity
                 .status(OK)
-                .body(GlobalApiResponse.of(data));
+                .body(ImageResponse.of(data));
     }
 
     @DeleteMapping
     @Operation(summary = "이미지 삭제", description = "이미지를 삭제합니다.")
-    public ResponseEntity<GlobalApiResponse> deleteImage(
+    public ResponseEntity<ImageResponse> deleteImage(
             @RequestBody ImageRequest imageRequest
     ) {
         imageService.deleteFile(imageRequest.imagePath());
         return ResponseEntity.status(NO_CONTENT).build();
+    }
+
+    @Builder
+    public record ImageResponse(
+            Integer status,
+            String message,
+            Object data,
+            List<Object> list
+    ) {
+        public static ImageResponse of(Object data) {
+            return ImageResponse.builder()
+                    .status(HttpStatus.OK.value())
+                    .message(null)
+                    .data(data)
+                    .list(null)
+                    .build();
+        }
     }
 }
