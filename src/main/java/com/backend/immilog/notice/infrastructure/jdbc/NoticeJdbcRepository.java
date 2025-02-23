74be0925f0e5a641afff3f30f6121ca5a56cf292
bzhs1992 <bzhs1992@icloud.com>
2025-02-23 11:42:20 +0900

    fix (notice) : 공지사항 조회결과가 0일때 예외 방지
    - 정상 케이스 이며 화면에서 문구로 처리함

--- src/main/java/com/backend/immilog/notice/infrastructure/jdbc/NoticeJdbcRepository.java
+++ src/main/java/com/backend/immilog/notice/infrastructure/jdbc/NoticeJdbcRepository.java
@@ -1,6 +1,7 @@
 package com.backend.immilog.notice.infrastructure.jdbc;
 
 import com.backend.immilog.notice.application.result.NoticeResult;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
@@ -41,24 +42,25 @@ public class NoticeJdbcRepository {
                 .list();
     }
 
-    public Long getTotal(
-            Long userSeq
-    ) {
+    public Long getTotal(Long userSeq) {
         String sql = """
-                SELECT n.*
+                SELECT COUNT(*) 
                 FROM notice n
-                LEFT JOIN notice_entity_target_Country ntc ON n.seq = ntc.notice_entity_seq
-                LEFT JOIN user u ON u.country = ntc.target_Country 
+                LEFT JOIN immilog.notice_entity_target_countries ntc ON n.seq = ntc.notice_entity_seq
+                LEFT JOIN user u ON u.country = ntc.target_countries 
                 LEFT JOIN notice_entity_read_users nru ON n.seq = nru.notice_entity_seq
-                WHERE (u.country = ntc.target_Country
-                   OR ntc.target_Country = 'ALL')
+                WHERE (u.country = ntc.target_countries
+                   OR ntc.target_countries = 'ALL')
                    AND n.status = 'NORMAL'
                    AND (nru.read_users IS NULL OR nru.read_users != ?)
                 """;
-        return jdbcClient.sql(sql)
-                .param(userSeq)
-                .query((rs, rowNum) -> rs.getLong(1))
-                .single();
+        try {
+            return jdbcClient.sql(sql)
+                    .param(userSeq)
+                    .query((rs, rowNum) -> rs.getLong(1))
+                    .single();
+        } catch (EmptyResultDataAccessException e) {
+            return 0L;
+        }
     }
-
 }
