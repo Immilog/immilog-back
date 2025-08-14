44be125fb2d7b65a89918e48fa95248dc9f03699
bzhs1992 <bzhs1992@icloud.com>
2025-08-14 23:48:45 +0900

    feat(common): UserInfo 객체, UserRole, UserStatus enum 추가

---
+++ immilog-common/src/main/java/com/backend/immilog/common/enums/UserRole.java
@@ -0,0 +1,36 @@
+package com.backend.immilog.common.enums;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+public enum UserRole {
+    ROLE_USER(
+            "ROLE_USER",
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+    ),
+
+    ROLE_ADMIN(
+            "ROLE_ADMIN",
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"))
+    );
+
+    private final String roleName;
+    private final List<GrantedAuthority> authorities;
+
+    UserRole(
+            String roleName,
+            List<GrantedAuthority> authorities
+    ) {
+        this.roleName = roleName;
+        this.authorities = authorities;
+    }
+
+    public boolean isAdmin() {
+        return this.equals(UserRole.ROLE_ADMIN);
+    }
+}
